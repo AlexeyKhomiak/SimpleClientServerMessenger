b20515e801cf41ab80b5f79ad8dde5e990ce361a
unknown <alexey.khomiak@gmail.com>
2018-08-15 22:34:53 +0300

    SimpleClientServerMessenger

---
+++ MessageExchange4.0/MessageExchange3.0/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageExchange3._0
+{
+    public partial class Form1 : Form
+    {
+        string ip = "127.0.0.1";
+        int port = 22316;
+        IPAddress ipAddr;
+        IPEndPoint endPoint;
+        Socket socket;
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add(new ColumnHeader());
+            listView1.Columns[0].Text = "Messages:";
+            listView1.Columns[0].Width =410;
+
+            ipAddr = IPAddress.Parse(ip);
+            endPoint = new IPEndPoint(ipAddr, port);
+            socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.IP);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (socket.Connected==false)
+            {
+                if (textBox1.Text != "")
+                {
+                    try
+                    {
+                        socket.Connect(endPoint);
+                        if (socket.Connected)
+                        {
+                            SendMessage("New client");
+
+                            listView1.Items.Add(ReceivedMessage()+ " You are connected!");
+
+                            SendMessage(textBox1.Text);
+
+                            char delimiter = ';';
+                            string[] substrings = ReceivedMessage().Split(delimiter);
+
+                            foreach (var item in substrings)
+                            {
+                                listBox1.Items.Add(item);
+                            }
+                            textBox1.Enabled = false;
+                            button1.Enabled = false;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите login!");
+                }
+            }
+        }        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "")
+            {
+                try
+                {
+                    if (socket.Connected)
+                    {
+                        if ((textBox3.Text != "") && (listBox1.SelectedIndex!=-1))
+                        {
+                            SendMessage("Send messages");
+                            SendMessage((listBox1.SelectedItem
+                                +";"+ DateTime.Now.ToLongTimeString() + ": "+ textBox1.Text + " > " + textBox3.Text+"&"));
+                            listView1.Items.Add((DateTime.Now.ToLongTimeString() +
+                                ": " + listBox1.SelectedItem + " < " + textBox3.Text));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Выбериле получателя и введите сообщение!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Подключитесь к серверу!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите login, подключитесь к серверу и выберите получателя!");
+            }
+            textBox3.Text = "";            
+        }
+
+        public void SendMessage(string s)
+        {
+            string msg = s;
+            byte[] sendBytes = Encoding.Default.GetBytes(msg);
+            socket.Send(sendBytes);
+        }
+
+        public string ReceivedMessage()
+        {
+            int bytesReceived = 0;
+            byte[] bytes = new byte[255];
+            bytesReceived = socket.Receive(bytes);
+            string res = Encoding.Default.GetString(bytes).TrimEnd('\0');
+            return res;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            socket.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            SendMessage("Get clients");
+
+            char delimiter = ';';
+            string[] substrings = ReceivedMessage().Split(delimiter);
+
+            foreach (var item in substrings)
+            {
+                if (item!="")
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                try
+                {
+                    if (socket.Connected)
+                    {
+                        SendMessage("Receive messages");
+                        SendMessage(textBox1.Text);
+                                                
+                        char delimiter = '&';
+                        string[] substrings = ReceivedMessage().Split(delimiter);
+
+                        for (int i = 0; i < substrings.Length; i++)
+                        {
+                            if (substrings[i]!="")
+                            {
+                                listView1.Items.Add((substrings[i]));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Подключитесь к серверу!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите login, подключитесь к серверу и выберите получателя!");
+            }
+        }
+    }
+}
---
+++ MessageExchange4.0/Server/ObjectServer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ObjectServer
+    {
+        string ip;
+        int port;
+        IPAddress ipAddr;
+        IPEndPoint endPoint;
+        Socket socket;
+        ObjectClient client;
+        public static List<ObjectClient> clients =new List<ObjectClient>();
+        public static Dictionary<string, string> messages;
+
+        public ObjectServer()
+        {
+            ip = "127.0.0.1";
+            port = 22316;
+            messages = new Dictionary<string, string>();
+            messages.Add("test", "test > Hello!");
+            ServerStart();
+        }
+        private void ServerStart()
+        {
+            ipAddr = IPAddress.Parse(ip);
+            endPoint = new IPEndPoint(ipAddr, port);
+            socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.IP);
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            Console.WriteLine("Server started...");
+            do
+            {
+                client = new ObjectClient(socket);
+                    clients.Add(client);
+                    Task.Factory.StartNew(() => ProcessClient(client, socket));
+            } while (true);
+        }
+
+        static void ProcessClient(ObjectClient client, Socket socket)
+        {
+            int bytesReceived = 0;
+            try
+            {
+                do
+                {
+                    byte[] bytes = new byte[255];
+                    bytesReceived = client.socketClient.Receive(bytes);
+                    string msg = Encoding.Default.GetString(bytes).TrimEnd('\0');
+                                                           
+                    if (msg== "New client")
+                    {
+                        string currentTime = DateTime.Now.ToLongTimeString();
+                        byte[] data = Encoding.Default.GetBytes(currentTime);
+                        client.socketClient.Send(data);
+
+                        byte[] bytes2 = new byte[255];
+                        bytesReceived = client.socketClient.Receive(bytes2);
+                        string msg2 = Encoding.Default.GetString(bytes2).TrimEnd('\0');
+                        client.name = msg2;
+
+                        string allClients = "";
+                        for (int i = 0; i < clients.Count; i++)
+                        {
+                            allClients += clients[i].name+";";
+                        }
+                        byte[] msgAllClients = Encoding.Default.GetBytes(allClients);
+                        client.socketClient.Send(msgAllClients);
+                    }
+                    else if (msg=="Get clients")
+                    {
+                        string allClients="";
+                        for (int i = 0; i < clients.Count; i++)
+                        {
+                            allClients += clients[i].name + ";";
+                        }
+                        byte[] msgAllClients = Encoding.Default.GetBytes(allClients);
+                        client.socketClient.Send(msgAllClients);
+                    }
+                    else if (msg == "Send messages")
+                    {                        
+                        byte[] bytes2 = new byte[255];
+                        bytesReceived = client.socketClient.Receive(bytes2);
+                        string msg2 = Encoding.Default.GetString(bytes2).TrimEnd('\0');
+
+                        char delimiter1 = ';';
+                        string[] mess = msg2.Split(delimiter1);
+
+                        if (messages.ContainsKey(mess[0]))
+                        {
+                            messages[mess[0]] += mess[1];
+                        }
+                        else
+                        {
+                            messages.Add(mess[0], (mess[1]));
+                        }
+                    }
+                    else if (msg == "Receive messages")
+                    {
+                        byte[] bytes2 = new byte[255];
+                        bytesReceived = client.socketClient.Receive(bytes2);
+                        string msg2 = Encoding.Default.GetString(bytes2).TrimEnd('\0');
+                 
+                        if (messages.ContainsKey(msg2))
+                        {
+                            byte[] data2 = Encoding.Default.GetBytes(messages[msg2]);
+                            client.socketClient.Send(data2);
+                            messages.Remove(msg2);
+                            messages.Remove("test");
+                        }
+                        else if (messages.ContainsKey("test"))
+                        {
+                            byte[] data2 = Encoding.Default.GetBytes(DateTime.Now.ToLongTimeString()
+                                +": "+messages["test"]);
+                            client.socketClient.Send(data2);
+                            messages.Remove("test");
+                        }
+                        else
+                        {
+                            byte[] data3 = Encoding.Default.GetBytes(DateTime.Now.ToLongTimeString() 
+                                +": "+ "no messages");
+                            client.socketClient.Send(data3);
+                        }
+                    }
+                } while (bytesReceived > 0);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message!="")
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                client.socketClient.Close();
+            }
+        }
+
+
+    }
+}
